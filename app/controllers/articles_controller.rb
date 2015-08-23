d4ae9b9d91a089a61d54d359f0b7ce65f080a689
Sylvia <sylviakieha@gmail.com>
2015-08-24 01:39:18 +0300

    showing articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def new
   end
 
@@ -8,8 +12,9 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to @article
   end
+
   private
   def article_params
-    params.require[:article].permit(:title, :text)
+    params.require(:article).permit(:title, :text)
   end
 end
